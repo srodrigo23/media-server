2858ccc45ccc6690bc27f6d59cc0679d313bbbe8
rodres101 <rodrigosergio93@gmail.com>
2023-09-24 15:11:35 -0400

    adding tailwind pug and initial style

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var express = require('express');
+let express = require('express');
+let path= require('path');
+let app = express();
 
-var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+    res.render('index');
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/**/*.{html,js,pug}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
+
