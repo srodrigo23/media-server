475a9fe2a9f284b50ed74feddf4ca1f709472904
rodres101 <rodrigosergio93@gmail.com>
2023-09-24 15:57:37 -0400

    tidy up my code

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ let express = require('express');
 let path= require('path');
 let app = express();
 
+console.log(__dirname)
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/**/*.{html,js,pug}'],
+  content: ['./src/views/**/*.{html,js,pug}'],
   theme: {
     extend: {},
   },
