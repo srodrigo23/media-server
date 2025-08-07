400cbb51bccc79e0de59b7f7df8b52df6c2f11e5
rodres101 <rodrigosergio93@gmail.com>
2025-08-07 17:58:02 -0400

    adding login form

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ To  init prisma ORM
 ```
 npx prisma init --datasource-provider postgresql
 ```
+
+Following this article
+[article 1](https://auth0.com/blog/create-a-simple-and-stylish-node-express-app/)
--- src/app.js
+++ src/app.js
@@ -1,17 +1,63 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let app = express();
+const express = require('express');
+const path = require('path');
+
+const session = require('express-session');
+const app = express();
+
+const fs = require('fs');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
+
+// Middleware to parse URL-encoded bodies (for form submissions)
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret:'a_secret_session',
+  resave:false,
+  saveUninitialized:false,
+  cookie:{ maxAge: 60*60*1000 } // this session expires in 1hr
+}))
 
 app.get('/', (req, res) => {
   res.redirect('/music');
 });
+
+app.get('/login', (req, res)=>{
+  res.render('login', {title: 'Login'});
+})
+
+app.post('/login', (req, res)=>{
+  const {username, password} = req.body
+  if(username === 'admin' && password==='admin'){
+    req.session.user = { username:username }
+    res.redirect('/music')
+  }else{
+    res.render('login', {error:'Usuario Inválido'})
+  }
+})
+
+app.get('/logout', (req, res)=>{
+  req.session.destroy((err)=>{
+    if(err){
+      console.error('Error destroying session:', err)
+      return res.redirect('/error')
+    }
+    res.clearCookie('connect.sid'); //clear the session cookie (if using connect.sid)
+    res.redirect('/login')
+  });
+});
+
+const isAuthenticated = (req, res, next) => {
+  if(req.session.user){
+    next();
+  }else{
+    res.redirect('/login')
+  }
+}
 
 const uploadMiddleware = require('./middlewares/upload');
 
@@ -37,19 +83,18 @@ app.post('/upload', uploadMiddleware, (req, res)=>{
   // res.status(200).json({ message: 'File upload successful' });
 })
 
-
-app.get('/music',(req, res) =>{
+app.get('/music', isAuthenticated, (req, res) =>{
   res.render('music', {'label':'music'});
 });
 
-app.get('/images',(req, res) =>{
+app.get('/images', isAuthenticated, (req, res) =>{
   res.render('images', {'label':'image'});
 });
 
-app.get('/video',(req, res) =>{
+app.get('/video', isAuthenticated, (req, res) =>{
   res.render('video', {'label':'video'});
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-});
+})
